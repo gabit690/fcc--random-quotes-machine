f57559520e7859b2b06d36c53222de1a095f62ad
gabit <gabit690@gmail.com>
2021-01-22 11:43:08 -0300

    Finalización de los puntos pedidos para las pruebas sin estilos definitivos.

--- src/QuoteBox.js
+++ src/QuoteBox.js
@@ -49,16 +49,21 @@ const quotes = [
   
 const getRandomNumber = (min, max) => Math.round((Math.random() * (max - min)) + min);
   
-let randomNumber = getRandomNumber(1, 10);
+let currentRandomNumber = getRandomNumber(1, 10);
 
 function QuoteBox(props) {
-
-  const [quote, setQuote] = useState(quotes[randomNumber - 1]);
+  const [quote, setQuote] = useState(quotes[currentRandomNumber - 1]);
 
   function changeQuote() {
     props.setColor();
-    randomNumber = getRandomNumber(1, 10);
-    setQuote(quotes[randomNumber - 1]);
+    let newRandomNumber;
+
+    do {
+      newRandomNumber = getRandomNumber(1, 10);
+    } while (newRandomNumber === currentRandomNumber);
+
+    currentRandomNumber = newRandomNumber;
+    setQuote(quotes[currentRandomNumber - 1]);
   }
 
   return (
@@ -66,7 +71,7 @@ function QuoteBox(props) {
       <p id="text">{quote.phrase}</p>
       <p id="author"><span>- </span>{quote.game}</p>
       <button id="new-quote" onClick={changeQuote}>New quote</button>
-      <a href="www.google.com" target="_blank" id="tweet-quote">
+      <a href={`https://twitter.com/intent/tweet?text="${quote.phrase}" - ${quote.game}&hashtags=${"quotes"}`} target="_blank" id="tweet-quote">
         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-twitter" viewBox="0 0 16 16">
           <path d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z"/>
         </svg>
